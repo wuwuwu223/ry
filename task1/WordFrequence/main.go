8a6518dc77aa75de95c7c2062f415fbf60145467
wuwuwu <wuwuwu223@qq.com>
2022-09-20 11:33:55 +0800

    完善个人项目

--- task1/WordFrequence/main.go
+++ task1/WordFrequence/main.go
@@ -1,52 +1,87 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"runtime"
 	"strings"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
 // 线程数=CPU核心数
-var treadNum = runtime.NumCPU()
+var treadNum = 20 * runtime.NumCPU()
+var stopWordMap = make(map[string]bool)
+var vWordMap = make(map[string]string)
 
 func main() {
 	menu()
 }
 
 func menu() {
-	fmt.Println("1.统计单词出现次数")
-	fmt.Println("2.统计字符出现次数")
-	fmt.Println("3.退出")
-	id := 0
-	fmt.Scan(&id)
-	switch id {
-	case 1:
-		//读取文件
-		str := ReadFile()
-		//开始时间
-		startTime := time.Now()
-		WordSort(str)
-		//结束时间
-		endTime := time.Now()
-		fmt.Println("总耗时：", endTime.Sub(startTime))
-	case 2:
-		fmt.Println("请输入字符串：")
-		var str string
-		fmt.Scan(&str)
-		CharSort(str)
-	case 3:
-		os.Exit(0)
-	default:
-		fmt.Println("输入错误")
+	//wf -c 文件名 -n 10
+	var fileName1 string
+	var fileName2 string
+	var filedir string
+	var stopWordsFile string
+	var vWordsFile string
+	var n int
+	flag.StringVar(&fileName1, "c", "default", "排序字符文件名")
+	flag.StringVar(&fileName2, "f", "default", "排序单词文件名")
+	flag.StringVar(&filedir, "d", "default", "排序单词文件夹")
+	flag.StringVar(&stopWordsFile, "x", "default", "停用词文件名")
+	flag.StringVar(&vWordsFile, "v", "default", "替换词文件名")
+	flag.IntVar(&n, "n", 0, "输出前n个单词/字符")
+	flag.Parse()
+
+	if stopWordsFile != "default" {
+		stopWordsStr := ReadFile(stopWordsFile)
+		stopWords := strings.Split(stopWordsStr, "\n")
+		for _, v := range stopWords {
+			stopWordMap[v] = true
+		}
 	}
+
+	if vWordsFile != "default" {
+		vWordsStr := ReadFile(vWordsFile)
+		arr1 := strings.Split(vWordsStr, "\n")
+		for i := range arr1 {
+			arr2 := strings.Split(arr1[i], " ")
+			for j := range arr2 {
+				vWordMap[arr2[j]] = arr2[0]
+			}
+		}
+	}
+
+	if fileName1 == "default" && fileName2 == "default" && filedir == "default" {
+		fmt.Println("请输入文件名或文件夹名")
+		return
+	}
+	if fileName1 != "default" && fileName2 == "default" && filedir == "default" {
+		str := ReadFile(fileName1)
+		CharSort(str, n)
+		return
+	}
+	if fileName2 != "default" && filedir == "default" {
+		str := ReadFile(fileName2)
+		WordSort(str, n)
+		return
+	}
+	if filedir != "default" {
+		str := ReadDir(filedir)
+		WordSort(str, n)
+		return
+	}
+
+	fmt.Println("请输入文件名或文件夹名")
 }
 
-func WordSort(str string) []WordFrequent {
+func WordSort(str string, n int) []WordFrequent {
 	str = strings.ToLower(str)
+	var wordMap sync.Map
+	var mutex sync.Mutex
 	var wordRank []WordFrequent
 	allWord := int64(0)
 
@@ -55,19 +90,11 @@ func WordSort(str string) []WordFrequent {
 	end := len(str) / treadNum
 	//遍历开始时间
 	startTime := time.Now()
+	if len(str) < treadNum {
+		treadNum = 1
+	}
 	for i := 0; i < treadNum; i++ {
 		wg.Add(1)
-		if str[end] != ' ' {
-			for str[end] >= 'a' && str[end] <= 'z' || str[end] >= 'A' && str[end] <= 'Z' || str[end] >= '0' && str[end] <= '9' {
-				//fmt.Println("end:", end)
-				end++
-				if end >= len(str) {
-					break
-				}
-			}
-		}
-		//fmt.Println("start:", start, "end:", end)
-		//fmt.Println(111)
 		if start != 0 {
 			start = end + 1
 			end = start + len(str)/treadNum
@@ -75,8 +102,19 @@ func WordSort(str string) []WordFrequent {
 				end = len(str)
 			}
 		}
+
 		if i == treadNum-1 {
 			end = len(str)
+		} else {
+			if str[end] != ' ' {
+				for str[end] >= 'a' && str[end] <= 'z' || str[end] >= 'A' && str[end] <= 'Z' || str[end] >= '0' && str[end] <= '9' || str[end] == '\'' {
+					//fmt.Println("end:", end)
+					end++
+					if end >= len(str) {
+						break
+					}
+				}
+			}
 		}
 		txt := str[start:end]
 		//fmt.Println("start:", start, "end:", end)
@@ -88,6 +126,7 @@ func WordSort(str string) []WordFrequent {
 					str = str[:j] + " " + str[j+1:]
 				}
 			}
+			//fmt.Println("str:", str)
 			//fmt.Println(i3, "替换完成")
 			arrSlice := strings.Split(str, " ")
 			for _, v := range arrSlice {
@@ -96,24 +135,21 @@ func WordSort(str string) []WordFrequent {
 						arr := strings.Split(v, "'")
 						v = arr[0]
 					}
-					//如果单词已经存在，频率+1
-					if len(wordRank) > 0 {
-						for i2 := 0; i2 < len(wordRank); i2++ {
-							if wordRank[i2].Word == v {
-								atomic.AddInt64(&wordRank[i2].Frequent, 1)
-								atomic.AddInt64(&allWord, 1)
-								break
-							}
-							if i2 == len(wordRank)-1 {
-								wordRank = append(wordRank, WordFrequent{v, 1})
-								atomic.AddInt64(&allWord, 1)
-								break
-							}
-						}
-					} else {
-						wordRank = append(wordRank, WordFrequent{v, 1})
-						atomic.AddInt64(&allWord, 1)
+					if _, ok := stopWordMap[v]; ok {
+						continue
 					}
+					if _, ok := vWordMap[v]; ok {
+						v = vWordMap[v]
+					}
+					//如果单词已经存在，频率+1
+					mutex.Lock()
+					allWord++
+					if value, ok := wordMap.Load(v); ok {
+						wordMap.Store(v, value.(int)+1)
+					} else {
+						wordMap.Store(v, 1)
+					}
+					mutex.Unlock()
 				}
 			}
 
@@ -125,16 +161,24 @@ func WordSort(str string) []WordFrequent {
 	//遍历结束时间
 	endTime := time.Now()
 	fmt.Println("遍历耗时：", endTime.Sub(startTime))
+	//把map转换成切片
+	wordMap.Range(func(key, value interface{}) bool {
+		wordRank = append(wordRank, WordFrequent{key.(string), int64(value.(int))})
+		return true
+	})
 
 	fmt.Println("排名\t单词\t出现次数\t出现频率")
 	wordRank = sortWordFrequence(wordRank)
-	for i := 0; i < 1000; i++ {
+	for i := range wordRank {
+		if n != 0 && i == n {
+			break
+		}
 		fmt.Printf("%d\t%s\t%d\t%.2f%%\n", i+1, wordRank[i].Word, wordRank[i].Frequent, float64(wordRank[i].Frequent)/float64(allWord)*100)
 	}
 	return wordRank
 }
 
-func CharSort(str string) []WordFrequent {
+func CharSort(str string, n int) []WordFrequent {
 	freqmap := make(map[string]int)
 	allChar := 0
 	//遍历开始时间
@@ -155,7 +199,10 @@ func CharSort(str string) []WordFrequent {
 
 	fmt.Println("字符\t出现次数\t出现频率")
 	charRank = sortWordFrequence(charRank)
-	for _, v := range charRank {
+	for i, v := range charRank {
+		if n != 0 && i >= n {
+			break
+		}
 		fmt.Printf("%s\t%d\t%.2f%%\n", v.Word, v.Frequent, float64(v.Frequent)/float64(allChar)*100)
 	}
 	return charRank
@@ -177,9 +224,9 @@ func sortWordFrequence(wf []WordFrequent) []WordFrequent {
 	return wf
 }
 
-func ReadFile() string {
+func ReadFile(filename string) string {
 	startTime := time.Now()
-	file, err := os.OpenFile("/Users/wuwuwu/soft-project/task1/WordFrequence/gone-with-the-wind.txt", os.O_RDONLY, 0666)
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0666)
 	if err != nil {
 		fmt.Println("文件打开失败")
 		fmt.Println(err)
@@ -194,6 +241,28 @@ func ReadFile() string {
 			break
 		}
 		str += string(buf[:n])
+	}
+	endTime := time.Now()
+	fmt.Println("读取文件耗时：", endTime.Sub(startTime))
+	return str
+}
+
+// readdir return string
+func ReadDir(dir string) string {
+	var str string
+	startTime := time.Now()
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Println("文件打开失败")
+		fmt.Println(err)
+		return ""
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			str += ReadDir(dir + "/" + file.Name())
+		} else {
+			str += " " + ReadFile(dir+"/"+file.Name())
+		}
 	}
 	endTime := time.Now()
 	fmt.Println("读取文件耗时：", endTime.Sub(startTime))
